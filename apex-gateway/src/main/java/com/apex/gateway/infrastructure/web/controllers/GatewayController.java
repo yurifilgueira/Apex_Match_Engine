25bb4b344d1c0838b96960a33ee970faeb90ec19
yurifilgueira <yurimedeiros141@gmail.com>
2026-03-16 23:07:41 -0300

    refactor: Remove logger and counters from GatewayController

--- apex-gateway/src/main/java/com/apex/gateway/infrastructure/web/controllers/GatewayController.java
+++ apex-gateway/src/main/java/com/apex/gateway/infrastructure/web/controllers/GatewayController.java
@@ -3,8 +3,6 @@ package com.apex.gateway.infrastructure.web.controllers;
 
 import com.apex.gateway.application.ingress.OrderPublisher;
 import com.apex.gateway.infrastructure.web.dto.OrderDTO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,15 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 @RestController
 @RequestMapping(value = "/orders")
 public class GatewayController {
 
     private final OrderPublisher publisher;
-    private final AtomicInteger counter = new AtomicInteger(0);
-    private final Logger logger = LoggerFactory.getLogger(GatewayController.class);
 
     public GatewayController(OrderPublisher publisher) {
         this.publisher = publisher;
@@ -31,12 +25,6 @@ public class GatewayController {
     public Mono<ResponseEntity<Void>> submit(@RequestBody OrderDTO orderDTO) {
         return Mono.fromRunnable(() -> publisher.publish(orderDTO))
                 .subscribeOn(Schedulers.boundedElastic())
-                .doOnSubscribe(s -> {
-                    counter.incrementAndGet();
-                    if (counter.get() % 10000 == 0) {
-                        logger.info("Published {} orders", counter.get());
-                    }
-                })
                 .thenReturn(ResponseEntity.accepted().build());
     }
 
